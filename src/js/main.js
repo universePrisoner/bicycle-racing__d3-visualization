e56a43e3ce875dfd9f0ec96e280c49863f3541a2
Anatoly Volgin <anatoly.volgin.rg@gmail.com>
2018-09-10 19:15:53 +0300

    0.0.1

--- src/js/main.js
+++ src/js/main.js
@@ -4,6 +4,25 @@ req.open( "GET", 'https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenc
 req.onload  =   function() {
 	const json = JSON.parse(req.responseText);
 	
+	let scales = {
+		'xScale'	: d3.linearScale().domain().range(),
+		'yScale'	: d3.linearScale().domain().range
+	}
+
+	let text		=	{
+		'title'		:	'Doping in Professional Bicycle Racing',
+		'subtitle'	:	'35 Fastest times up Alpe d\'Hues',
+		'groups'	:	{
+							'noDoping'	: 'No doping allegations',
+							'doping'	: 'Riders with doping allegations'
+						},
+		'axes'		:	{
+						'yAxis'	: 'Time in Minutes'
+						}
+					}
+	const COLORS	=	{
+					'white'		: '#FFFFFF'
+					}
 }
 
 req.send();
