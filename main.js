25f2ce8438f4ad6a61ac0ed2fa9befb604fa4733
Anatoly Volgin <anatoly.volgin.rg@gmail.com>
2018-09-17 15:51:41 +0300

    1.1.1

--- main.js
+++ main.js
@@ -22,7 +22,10 @@ xhr.onreadystatechange = function() {
 				'bottom': 50
 			}
 		}
-
+		let legends = {
+			'legend-1' : 'No doping allegations',
+			'legend-2' : 'Riders with doping allegations'
+		}
 		let circleBackground = d3.scaleOrdinal(d3.schemeCategory10);
 
 		// Convert a time race to actual date for each element
@@ -47,42 +50,81 @@ xhr.onreadystatechange = function() {
 		let xAxis = d3.axisBottom(xScale).tickFormat(d3.format('d'));
 		let yAxis = d3.axisLeft(yScale).tickFormat(d3.timeFormat('%M:%S'));
 
-		let svg		= d3.select('body').append('svg').attr('class', 'canvas').attr('width', DIMENSIONS.canvas.width).attr('height', DIMENSIONS.canvas.height);
-		let chart	= svg.append('g').attr('class', 'chart').attr('transform', 'translate(' + DIMENSIONS.margins.left + ', ' + DIMENSIONS.margins.top + ')');
-		let tooltip = d3.select('body').append('div').attr('class', 'tooltip');
+		let svg			= d3.select('body').append('svg').attr('class', 'canvas').attr('width', DIMENSIONS.canvas.width).attr('height', DIMENSIONS.canvas.height);
+		let chart		= svg.append('g').attr('class', 'chart').attr('transform', 'translate(' + DIMENSIONS.margins.left + ', ' + DIMENSIONS.margins.top + ')');
+		let tooltip 	= d3.select('body').append('div').attr('class', 'tooltip').attr('id', 'tooltip');
+		let title 		= d3.select('body').append('h1').attr('id', 'title').attr('class', 'title').text('Doping in Professional Bicycle Racing');
+		let subTitle 	= d3.select('body').append('h2').attr('class', 'subtitle').text('35 Fastest times up Alpe d\'Huez');
 
 		// Append the axes at the svg
 		svg.append('g')
 			.call(xAxis)
 			.attr('id', 'x-axis')
+			.attr('transform', 'translate(' + DIMENSIONS.margins.left + ', ' + ( DIMENSIONS.canvas.height * 2 ) + ')' )
+			.style('opacity', 0)
+			.transition()
+			.duration(1000)
+			.style('opacity', 1)
 			.attr('transform', 'translate(' + DIMENSIONS.margins.left + ', ' + ( DIMENSIONS.canvas.height - DIMENSIONS.margins.bottom ) + ')' );
 		svg.append('g')
 			.call(yAxis)
 			.attr('id', 'y-axis')
+			.attr('transform', 'translate(-' + DIMENSIONS.margins.left * 5 + ', ' + DIMENSIONS.margins.top + ')')
+			.style('opacity', 0)
+			.transition()
+			.duration(1000)
+			.style('opacity', 1)
 			.attr('transform', 'translate(' + DIMENSIONS.margins.left + ', ' + DIMENSIONS.margins.top + ')');
 
 		chart.selectAll('circle')
 			.data(json)
 			.enter()
+			.append('a') // Make circle as link to more information about a biker
+			.attr('href', (d) => d.URL)
+			.attr('target', '_blank')
 			.append('circle')
 			.attr('class', 'dot')
 			.attr('data-xvalue', (d) => d.Year)
 			.attr('data-yvalue', (d) => d.Time)
-			.attr('r', 8)
+			.attr('r', 0)
 			.attr('cx', (d) => xScale(d.Year))
 			.attr('cy', (d) => yScale(d.Time))
 			.attr('stroke', '#000000')
 			.attr('fill', (d) => circleBackground(d.Doping == '')) // Fill circle depending on the doping test.
 			.on('mouseover', (d) => {
-				tooltip.style('left', (d3.event.pageX) + 5 + 'px').style('top', (d3.event.pageY) + 5 + 'px').style('opacity', 0.8);
-				tooltip.text( '15:30' );
+				tooltip	.attr('data-year', d.Year)
+						.style('left', (d3.event.pageX) + 5 + 'px')
+						.style('top', (d3.event.pageY) + 5 + 'px')
+						.style('opacity', 0.9)
+						.style('background', () => circleBackground(d.Doping == '')) // Change background depending on the doping test.
+						.html( `
+							<span class="tooltip__row-title tooltip__name">Name:</span> ${ d.Name }<br/>
+							<span class="tooltip__row-title tooltip__year">Year:</span> ${ d.Year } <br/>
+							<span class="tooltip__row-title tooltip__time">Record:</span> ${ d.Time.getMinutes() }:${ d.Time.getSeconds() } <br/>
+							<span class="tooltip__tips">Click on the circle to read more...</span>
+						` );
 			} )
 			.on('mousemove', (d) => {
 				tooltip.style('left', (d3.event.pageX) + 5 + 'px').style('top', (d3.event.pageY) + 5 + 'px');
 			})
 			.on('mouseout', (d) => {
 				tooltip.style('left', -1000).style('top', -1000).style('opacity', 0);
-			});
+			})
+			.transition()
+			.delay((d, i) => i * 50 )
+			.attr('r', 6);
+
+		let legend1 = svg.append('g').attr('id','legend').attr('transform', 'translate(850, 100)');
+		legend1.append('text').text(legends['legend-1']).attr('transform', 'translate(30, 15)');
+		legend1.append('rect').attr('x', 0).attr('y', 0).attr('width', 20).attr('height', 20).style('fill', circleBackground(false));
+		legend1.style('opacity', 0).transition().duration(1).style('opacity', 1);
+
+		let legend2 = svg.append('g').attr('id','legend').attr('transform', 'translate(850, 125)');
+		legend2.append('text').text(legends['legend-2']).attr('transform', 'translate(30, 15)');
+		legend2.append('rect').attr('x', 0).attr('y', 0).attr('width', 20).attr('height', 20).style('fill', circleBackground(true));
+		legend2.style('opacity', 0).transition().duration(1).style('opacity', 1);
+
+		let yAxisLabel = svg.append('text').text('Time in Minutes').attr('transform', 'rotate(-90), translate(-200, 50)');
 
 		return false;
 	}
